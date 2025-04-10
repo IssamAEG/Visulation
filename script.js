66d18452b5222c3a25504ea02b0e5faa4b181abe
Sajeevan10 <ssajeevan20@hotmail.com>
2025-04-10 22:01:17 +0200

    premiere partie du script js

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ const projection = d3.geoNaturalEarth1()
     .translate([width / 2, height / 2]);
 
 const path = d3.geoPath().projection(projection);
+
+// Charger les données géographiques (GeoJSON)
+d3.json("data/custom.geo.json").then(function(geoData) {
+
+    // Charger les données des attaques numériques
+    d3.csv("data/attacks_data.csv").then(function(data) {
+
+        // Préparer les données des attaques par pays
+        const attackData = {};
+        data.forEach(function(d) {
+            if (!attackData[d.country]) {
+                attackData[d.country] = [];
+            }
+            attackData[d.country].push({
+                type: d.attack_type,
+                count: +d.count
+            });
+        });
